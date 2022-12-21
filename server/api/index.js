c30737669f797a59421693a2f5a3be122c4082ef
Joe Holder <jholder090@yahoo.com>
2022-12-21 13:33:22 -0700

    server and webpack running

---
+++ server/api/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+// /api
+// router.use('/users', require('./users'));
+// router.use('/notes', require('./notes'));
+
+// router.use(function (req, res, next) {
+//   const err = new Error('Not found.');
+//   err.status = 404;
+//   next(err);
+// });
+
+module.exports = router;
---
+++ server/api/notes.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+
+// api/notes
+// router.get('/', (req, res, next))
